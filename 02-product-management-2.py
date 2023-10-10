1e007d47a45bb3ab59c341fdfbe11f4512cbf986
Gustavo Pacheco <ryctabo@gmail.com>
2023-10-09 22:31:25 -0500

    add comments

--- 02-product-management-2.py
+++ 02-product-management-2.py
@@ -16,6 +16,9 @@ CAMPOS
 
 product_list = []
 
+# 1. crear un diccionario llamado product
+# 2. llenar los datos necesarios dentro del diccionario product
+# 3. agregar el diccionario creado dentro de una lista
 def create_product():
     product = {}
     product["code"] = input("Código del producto: ")
@@ -26,6 +29,9 @@ def create_product():
     product_list.append(product)
     print()
 
+# 1. preguntando si la lista tiene elementos
+# 1.a si no tiene elementos imprimir la frase "La lista está vacia."
+# 2. recorrer la lista de productos e imprimir cada uno de los valores que tiene el diccionario
 def find_all_products():
     if len(product_list) == 0:
         print("La lista está vacia.")
@@ -40,18 +46,23 @@ def find_all_products():
     print()
 
 while True:
+    # 1. creando un menú para administrar la lista de productos
     print("1. Agregar un producto")
     print("2. Consultar un producto")
     print("3. Salir")
 
+    # 2. preguntando por una opción
     option = input("Selecciona una opción: ")
 
+    # 3. Salimos del programa?
     if option == "3":
         print("Good bye! :)")
         break
 
+    # Creamos productos?
     if option == "1":
         create_product()
 
+    # Consultamos todos los productos agregados?
     if option == "2":
         find_all_products()
